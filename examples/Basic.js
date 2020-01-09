1fd127ee4d98df75cc7acf3ba4f6c519f8b35bf0
YanceyOfficial <yanceyofficial@gmail.com>
2020-01-09 16:50:33 +0800

    feat: delete useless files

--- examples/Basic.js
+++ examples/Basic.js
@@ -10,3 +10,40 @@ console.log(cost(100))
 console.log(cost(200))
 console.log(cost(300))
 console.log(cost())
+
+function Plus() {
+  this.value = 1
+}
+
+Plus.prototype.add = function() {
+  this.value++
+  console.log(this.value)
+}
+
+const plus = new Plus()
+
+plus.add()
+plus.add()
+plus.add()
+plus.add()
+plus.add()
+
+const p = plus1()
+p()
+p()
+p()
+p()
+p()
+
+const o = {
+  value: 1,
+  add() {
+    this.value++
+    console.log(this.value)
+  },
+}
+
+o.add()
+o.add()
+o.add()
+o.add()
